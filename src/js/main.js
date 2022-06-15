c371b630f804b2575b230f82b416b5f69e0bbc74
filipfit <filipfitzermann@gmail.com>
2022-06-15 12:29:56 +0200

    sync

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import * as d3 from 'd3';
+import { text } from 'd3';
 import { checkboxList } from './components/checkboxList';
 import { fileList, getCheckedFile } from './components/fileList';
 import { getCheckedBoxes } from './components/formInputCheckbox';
 import form_inputs from './json/form_inputs.json';
 import request_list from './json/request_list.json';
+import { histogram } from './visualization/Histogram';
 import { TextMarker } from './visualization/TextMarker';
 
 const URL = 'http://localhost:4567';
@@ -14,61 +16,20 @@ const SUBMIT_BUTTON = d3.select('#btn--submit');
 const VIS_ANCHOR = d3.select('#vis-anchor');
 let DEBUG_BUTTON = d3.select('#debug-btn');
 
-let data;
-
-function histogram(data) {
-  const height = 500;
-  const values = Object.values(data);
-  const barWidth = 20;
-  const margin = 1;
-
-  let scale = d3
-    .scaleLinear()
-    .domain([d3.min(values), d3.max(values)])
-    .range([0, 500]);
-
-  console.log(scale(values[0]));
-  console.log(values, d3.min(values));
-
-  let svg = d3
-    .create('svg')
-    .attr('height', height)
-    .attr('width', barWidth * values.length);
-
-  let group = svg
-    .selectAll('g')
-    .data(values)
-    .enter()
-    .append('g')
-    .attr('transform', function (d, i) {
-      return `translate(${i * barWidth}, 0)`;
-    });
-
-  group
-    .append('rect')
-    .attr('height', function (d) {
-      return scale(d);
-    })
-    .attr('width', barWidth - margin);
-
-  // group.append('text').attr('y', function (d) {
-  //   return scale(d);
-  // });
-
-  return svg.node();
-}
-
-axios
+/* axios
   .get(
     'http://localhost:4567/histogram?type=pos&file=text/xmi/18001.xmi.gz.xmi.gz&filter=100'
   )
   .then((res) => {
-    data = res.data.result;
+    const data = res.data.result;
+    console.log(data);
+
+    histogram(data);
 
     VIS_ANCHOR.append(() => histogram(data));
-  });
+}); */
 
-/* fileList().then((d) => {
+fileList().then((d) => {
   FILE_SELECTION_ANCHOR.append(() => d);
 });
 
@@ -109,4 +70,3 @@ function collectFormData() {
   const checkboxes = getCheckedBoxes();
   return { vis: checkboxes, file: file };
 }
- */
